33fcbffdbe3041697dc394dd10949931b7187107
Shaik-Muj <mesmh0810@gmail.com>
2026-07-15 22:16:04 +0530

    Schema changes done, alembic mirror

--- backend/models.py
+++ backend/models.py
@@ -4,10 +4,18 @@ from typing import Optional
 from sqlmodel import SQLModel, Field
 
 
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    username: str = Field(index=True, unique=True)
+    email: str = Field(unique=True)
+    hashed_password: str
+
+
 class Memory(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
 
-    user_id: str
+    user_id: int = Field(foreign_key="user.id")
     content: str
     source: str
 
